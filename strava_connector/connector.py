a8873b1ce76de54573343569a85c6f851fda8a8f
Thomas Camminady <0milieux_member@icloud.com>
2024-04-29 13:52:46 +0200

    Add retrieve activity functionality.

--- strava_connector/connector.py
+++ strava_connector/connector.py
@@ -20,8 +20,21 @@ class Connector:
     def get_logged_in_athlete_activities(
         self, page: int, per_page: int = 200
     ) -> dict:
-        """List Athlete Activities."""
-        params = f"""per_page={per_page}&page={page}&access_token={self.access_token}"""
-        url = f"{self.apiurl}/athlete/activities?{params}"
+        """List Athlete Activities.
+
+        https://developers.strava.com/docs/reference/#api-Activities-getLoggedInAthleteActivities
+        """
+        params = f"""per_page={per_page}&page={page}"""
+        url = f"{self.apiurl}/athlete/activities?{params}&access_token={self.access_token}"
+        result = requests.get(url, timeout=self.authenticator._timeout)
+        return result.json()
+
+    def get_activity_by_id(self, id: int, include_all_efforts: bool) -> dict:
+        """Get Activity.
+
+        https://developers.strava.com/docs/reference/#api-Activities-getActivityById
+        """
+        params = f"""include_all_efforts={include_all_efforts}"""
+        url = f"{self.apiurl}/activities/{id}?{params}&access_token={self.access_token}"
         result = requests.get(url, timeout=self.authenticator._timeout)
         return result.json()
